1c38017747de3c04d0a3c572b3caf24b56d937ab
yqianjiang <y.qian.jiang@hotmail.com>
2020-10-26 14:30:52 +0800

    update HH model

--- nbmodels/neurons/HH.py
+++ nbmodels/neurons/HH.py
@@ -1,5 +1,5 @@
-import npbrain as nb
-import npbrain.numpy as np
+import brainpy as bp
+import brainpy.numpy as np
 
 # constant values
 E_NA = 50.
@@ -11,9 +11,9 @@ G_K = 36.
 G_LEAK = 0.03
 V_THRESHOLD = 20.
 
-NOISE = 0.
+NOISE = 1.
 
-def HH (noise=NOISE, V_threshold = V_THRESHOLD, C = C, E_Na = E_NA, E_K = E_K,
+def define_HH (noise=NOISE, V_threshold = V_THRESHOLD, C = C, E_Na = E_NA, E_K = E_K,
              E_leak = E_LEAK, g_Na = G_NA, g_K = G_K, g_leak = G_LEAK):
     '''
     A Hodgkin–Huxley neuron implemented in NumpyBrain.
@@ -35,7 +35,7 @@ def HH (noise=NOISE, V_threshold = V_THRESHOLD, C = C, E_Na = E_NA, E_K = E_K,
     '''
     
     # define variables and initial values
-    ST = nb.types.NeuState(
+    ST = bp.types.NeuState(
         {'V': -65., 'm': 0.05, 'h': 0.60, 'n': 0.32, 'spike': 0., 'input': 0.},
         help='Hodgkin–Huxley neuron state.\n'
              '"V" denotes membrane potential.\n'
@@ -47,33 +47,33 @@ def HH (noise=NOISE, V_threshold = V_THRESHOLD, C = C, E_Na = E_NA, E_K = E_K,
     )
     
     
-    # call nb.integrate to solve the differential equations
+    # call bp.integrate to solve the differential equations
     
-    @nb.integrate
+    @bp.integrate
     def dmdt(m, t, V):
         alpha = 0.1 * (V + 40) / (1 - np.exp(-(V + 40) / 10))
         beta = 4.0 * np.exp(-(V + 65) / 18)
         return alpha * (1 - m) - beta * m
     
-    @nb.integrate
+    @bp.integrate
     def dhdt(h, t, V):
         alpha = 0.07 * np.exp(-(V + 65) / 20.)
         beta = 1 / (1 + np.exp(-(V + 35) / 10))
         return alpha * (1 - h) - beta * h
     
-    @nb.integrate
+    @bp.integrate
     def dndt(n, t, V):
         alpha = 0.01 * (V + 55) / (1 - np.exp(-(V + 55) / 10))
         beta = 0.125 * np.exp(-(V + 65) / 80)
         return alpha * (1 - n) - beta * n
     
-    @nb.integrate(noise = noise / C)
+    @bp.integrate
     def dVdt(V, t, m, h, n, input_current):
         I_Na = (g_Na * np.power(m, 3.0) * h) * (V - E_Na)
         I_K = (g_K * np.power(n, 4.0))* (V - E_K)
         I_leak = g_leak * (V - E_K)
         dvdt = (- I_Na - I_K - I_leak + input_current)/C 
-        return dvdt
+        return dvdt, noise / C
     
     # update the variables change over time (for each step)
     def update(ST, _t_):
@@ -89,31 +89,46 @@ def HH (noise=NOISE, V_threshold = V_THRESHOLD, C = C, E_Na = E_NA, E_K = E_K,
         ST['n'] = n
         ST['input'] = 0.   
     
-    return nb.NeuType(name='HH_neuron', requires={"ST": ST}, steps=update, vector_based=True)
+    return bp.NeuType(name='HH_neuron', requires={"ST": ST}, steps=update, vector_based=True)
 
 
-def get_neuron(geometry, monitors=['spike', 'V', 'm', 'h', 'n'], pars_update = {}, **kwargs):
+def get_neuron(geometry, monitors=['spike', 'V', 'm', 'h', 'n'], V_threshold = V_THRESHOLD, C = C, E_Na = E_NA, E_k = E_K,
+             E_leak = E_LEAK, g_Na = G_NA, g_K = G_K, g_leak = G_LEAK):
     '''
     Create HH neuron.
 
     Args:
         geometry (tuple): numbers of neurons to create, can be one or two dimensional.
         monitors (list): variables to record. Default = ['spike', 'V', 'm', 'h', 'n'].
-        pars_update (dict): parameters to be updated. Default = {}.
 
     Returns:
         neuron (NeuGroup): type of neurons.
     '''
-    if kwargs:
-        pars_update = kwargs
+    
+    mode = bp.profile._backend
 
-    neuron = nb.NeuGroup(HH, geometry=geometry, 
-                     monitors=monitors, 
-                     pars_update=pars_update)
+    if mode == 'numba':
+        HH = define_HH(noise=NOISE)
+    else:
+        HH = define_HH(noise=NOISE, V_threshold = V_threshold, C = C, E_Na = E_Na, E_K = E_k,
+             E_leak = E_leak, g_Na = g_Na, g_K = g_K, g_leak = g_leak)
+
+    neuron = bp.NeuGroup(HH, geometry=geometry, monitors=monitors)
+
+    if mode == 'numba':
+        neuron.par['E_Na'] = E_Na
+        neuron.par['E_K'] = E_k
+        neuron.par['E_leak'] = E_leak
+        neuron.par['g_Na'] = g_Na
+        neuron.par['g_K'] = g_K
+        neuron.par['g_leak'] = g_leak
+        neuron.par['C'] = C
+        neuron.par['V_threshold'] = V_threshold
                         
     return neuron
 
-def simulate(input_current, duration, geometry = (1,), monitors=['spike', 'V', 'm', 'h', 'n'], **kwargs):
+def simulate(input_current, duration, geometry = (1,), init_V = None, monitors=['spike', 'V', 'm', 'h', 'n'], V_threshold = V_THRESHOLD, C = C, E_Na = E_NA, E_k = E_K,
+             E_leak = E_LEAK, g_Na = G_NA, g_K = G_K, g_leak = G_LEAK):
     '''
     Apply input current to HH neurons.
 
@@ -121,6 +136,7 @@ def simulate(input_current, duration, geometry = (1,), monitors=['spike', 'V', '
         input_current (NPArray): amplitude of the input current.
         duration (float): duration of the input current.
         geometry (tuple): numbers of neurons to create, can be one or two dimensional.
+        init_V (float): you can specify the intial value of membrane potential.
         monitors (list): variables to record. Default = ['spike', 'V', 'm', 'h', 'n'].
 
     Returns:
@@ -133,13 +149,19 @@ def simulate(input_current, duration, geometry = (1,), monitors=['spike', 'V', '
 
     '''
 
-    HH_neuron = get_neuron(geometry=geometry, monitors=monitors, pars_update = kwargs)
+    HH_neuron = get_neuron(geometry=geometry, monitors=monitors, V_threshold = V_threshold, C = C, E_Na = E_Na, E_k = E_k,
+             E_leak = E_leak, g_Na = g_Na, g_K = g_K, g_leak = g_leak)
 
-    net = nb.Network(HH_neuron)
+    # set initial values
+    if init_V == None:
+        HH_neuron.ST['V'] = np.random.random(geometry) * 20 + -75
+    else:
+        HH_neuron.ST['V'] = init_V
 
-    net.run(duration=duration, inputs=[HH_neuron, 'ST.input', input_current], report=True)
+    # run simulation
+    HH_neuron.run(duration=duration, inputs=['ST.input', input_current], report=True)
 
-    ts = net.ts
+    ts = HH_neuron.mon.ts
     V = HH_neuron.mon.V[:, 0]
     m = HH_neuron.mon.m[:, 0]
     h = HH_neuron.mon.h[:, 0]
