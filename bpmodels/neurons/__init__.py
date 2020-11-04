c7e03d5c4e459b91b27031c8d4bbbbd8d02e2632
yqianjiang <y.jiang.psy@gmail.com>
2020-11-04 19:44:10 +0800

    update __init__.py

--- bpmodels/neurons/__init__.py
+++ bpmodels/neurons/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .HH import *
-from .LIF_model import get_LIF
+from .HH_model import *
+from .LIF_model import *
 from .Izhikevchi_model import *
-from .ExpIF_model import get_ExpIF
+from .ExpIF_model import *
