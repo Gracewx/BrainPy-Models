033da3308efaca8c827fedddfb1ba32a071a50bd
LiuXieyi <2001111580@stu.pku.edu.cn>
2020-11-14 18:56:23 +0800

    Add exponential & two_exponentials

---
+++ bpmodels/synapses/exponential.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import brainpy as bp
+import brainpy.numpy as np
+
+def get_exponential(g_max=0.2, E=-60., tau=8):
+    '''
+        exponential synapse model.
+
+        Args:
+            g_max (float): The peak conductance change in µmho (µS).
+            E (float): The reversal potential for the synaptic current.
+            tau (float): The time to peak of the conductance change.
+
+        Returns:
+            bp.Neutype: return description of exponential synapse model.
+    '''
+
+    requires = {
+        'ST': bp.types.SynState(['g'],help='The conductance defined by exponential function.'),
+        'pre': bp.types.NeuState(['V'], help='pre-synaptic neuron state must have "V"'),
+        'post': bp.types.NeuState(['input', 'V'], help='post-synaptic neuron state must include "input" and "V"')
+    }
+
+    def update(ST, _t_):
+            ST['g'] = g_max * np.exp(-(_t_ - tau) / tau)
+
+    @bp.delayed
+    def output(ST, _t_, pre, post):
+        I_syn = ST['g'] * (pre['V'] - E)
+        post['input'] += I_syn
+
+    return bp.SynType(name='exponential_synapse',
+                      requires=requires,
+                      steps=(update, output),
+                      vector_based=False)
