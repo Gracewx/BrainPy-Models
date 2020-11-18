0e744358fee2d53eb409b3b1dff97b6d3c2414f0
yqianjiang <y.jiang.psy@gmail.com>
2020-11-18 08:17:12 +0800

    update NMDA.py

--- bpmodels/synapses/NMDA.py
+++ bpmodels/synapses/NMDA.py
@@ -4,24 +4,24 @@ import brainpy as bp
 import brainpy.numpy as np
 
 
-def get_NMDA(g_max=0.15, E=0, alpha=0.062, beta=3.75, cc_Mg=1.2, tau_decay=100., a=0.5, tau_rise=2.):
+def get_NMDA(g_max=0.15, E=0, alpha=0.062, beta=3.57, cc_Mg=1.2, tau_decay=100., a=0.5, tau_rise=2.):
     """NMDA conductance-based synapse.
 
     .. math::
 
-        I_{syn}&=\\bar{g}_{syn} s (V-E_{syn})
+        I_{syn} &= \\bar{g}_{syn} s (V-E_{syn})
 
-        g(_t_) &=\\bar{g} \\cdot (V-E_{syn}) \\cdot g_{\\infty}
-        \\cdot \\sum_j s_j(_t_) \\quad (3)
+        g(t) &= \\bar{g} \\cdot (V-E_{syn}) \\cdot g_{\\infty}
+        \\cdot \\sum_j s_j(t)
 
-        g_{\\infty}(V,[{Mg}^{2+}]_{o}) & =(1+{e}^{-\\alpha V}
-        [{Mg}^{2+}]_{o}/\\beta)^{-1}  \\quad (4)
+        g_{\\infty}(V,[{Mg}^{2+}]_{o}) &= (1+{e}^{-\\alpha V}
+        [{Mg}^{2+}]_{o}/\\beta)^{-1} 
 
-        \\frac{d s_{j}(_t_)}{dt} & =-\\frac{s_{j}(_t_)}
-        {\\tau_{decay}}+a x_{j}(_t_)(1-s_{j}(_t_))  \\quad (5)
+        \\frac{d s_{j}(t)}{dt} &= -\\frac{s_{j}(t)}
+        {\\tau_{decay}}+a x_{j}(t)(1-s_{j}(t)) 
 
-        \\frac{d x_{j}(_t_)}{dt} & =-\\frac{x_{j}(_t_)}{\\tau_{rise}}+
-        \\sum_{k} \\delta(_t_-t_{j}^{k})  \\quad (6)
+        \\frac{d x_{j}(t)}{dt} &= -\\frac{x_{j}(t)}{\\tau_{rise}}+
+        \\sum_{k} \\delta(t-t_{j}^{k})
 
     where the decay time of NMDA currents is taken to be :math:`\\tau_{decay}` =100 ms,
     :math:`a= 0.5 ms^{-1}`, and :math:`\\tau_{rise}` =2 ms (Hestrin et al., 1990;
